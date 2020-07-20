fc2e22b2bce277dc2d372924f585b2bcd61cf61f
unknown <sneha.jayashankara@gmail.com>
2020-07-20 20:08:34 +0530

    inital commit

--- p1/settings.py
+++ p1/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATE_DIR="D:\\projects\\p1\\p1\\template"
+TEMPLATE_DIR=os.path.join(BASE_DIR,"template")
 
 
 # Quick-start development settings - unsuitable for production
